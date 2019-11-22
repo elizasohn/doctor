2f45c89e8b476f5c875732dee66f087583ccdf7f
Eliza Sohn <eliza.sohn@gmail.com>
2019-11-22 09:52:45 -0800

    add user interface that gets value of symptom input and returns a doctor

--- src/main.js
+++ src/main.js
@@ -2,3 +2,25 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import { Symptom } from './symptom.js';
+
+$(document).ready(function(){
+  $('.searchForm').submit(function(event){
+    event.preventDefault();
+    let symptomInput = $('input#symptom').val();
+    console.log(symptomInput);
+
+    (async () => {
+      let symptom = new Symptom();
+      console.log(symptom);
+      const response = await
+      symptom.getSymptom(symptomInput);
+      getElements(response);
+      console.log(response);
+    })();
+
+    function getElements(response) {
+      $(".output").text(`The doctors who can save your life are: ${response.data[0].practices[0].name}` );
+    }
+  });
+});
