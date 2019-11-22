4f0e2109fa89565661b2d7a390b08e3294e32fbf
Eliza Sohn <eliza.sohn@gmail.com>
2019-11-22 11:23:08 -0800

    assign variables to response output

--- src/main.js
+++ src/main.js
@@ -20,15 +20,23 @@ $(document).ready(function(){
     })();
 
     function getElements(response) {
-      $(".output").html(`<ul>The doctors who can save your life are:<ul><li> ${response.data[0].practices[0].name}</li>
-      <li>${response.data[0].practices[0].visit_address.street}
-          ${response.data[0].practices[0].visit_address.street2}</li>
-      <li>${response.data[0].practices[0].visit_address.city},
-          ${response.data[0].practices[0].visit_address.state}
-          ${response.data[0].practices[0].visit_address.zip}</li>
-      <li>phone number: ${response.data[0].practices[0].phones[0].number}</li>
-      <li>website: ${response.data[0].practices[0].website}</li>
-      <li>accepts new patients: ${response.data[0].practices[0].accepts_new_patients}</li>`);
+      let name = `${response.data[0].practices[0].name}`;
+      let street = `${response.data[0].practices[0].visit_address.street}`;
+      let street2 = `${response.data[0].practices[0].visit_address.street2}`;
+      let city = `${response.data[0].practices[0].visit_address.city}`;
+      let state = `${response.data[0].practices[0].visit_address.state}`;
+      let zip =  `${response.data[0].practices[0].visit_address.zip}`;
+      let number = `${response.data[0].practices[0].phones[0].number}`;
+      let website = `${response.data[0].practices[0].website}`;
+      let patients = `${response.data[0].practices[0].accepts_new_patients}`;
+
+      $(".output").html(`<ul>The doctors who can save your life are:<ul><li>` +
+        name + `</li>
+      <li>`+ street + `, ` + street2 + `</li>
+      <li>` + city + `, ` + state + ` ` + zip + `</li>
+      <li>phone number: ` + number + `</li>
+      <li>website: ` + website + `</li>
+      <li>accepts new patients: ` + patients + `</li>`);
     }
   });
 });
