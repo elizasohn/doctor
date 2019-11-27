1b0216dbad08b0c62e668fa46f80cec768e4b391
Student <example@example.com>
2019-11-26 16:54:54 -0800

    separate forEach and for loops into listing.js and export, import loops into main, import jquery into listing.js

--- src/listing.js
+++ src/listing.js
@@ -1,3 +1,5 @@
+import $ from 'jquery';
+
 export class Listing {
   constructor(name, street, street2, city, state, zip, number, website, patients) {
     this.name = name;
@@ -9,5 +11,32 @@ export class Listing {
     this.number = number;
     this.website = website;
     this.patients = patients;
+  }
+}
+
+export function getListings(response) {
+  let listings = [];
+  response.data.forEach(function(doctor){
+    listings.push(new Listing(
+    doctor.practices[0].name,
+    doctor.practices[0].visit_address.street,
+    doctor.practices[0].visit_address.street2,
+    doctor.practices[0].visit_address.city,
+    doctor.practices[0].visit_address.state,
+    doctor.practices[0].visit_address.zip,
+    doctor.practices[0].phones[0].number,
+    doctor.practices[0].website,
+    doctor.practices[0].accepts_new_patients,
+      )
+    );
+
+  });
+   for (let i = 0; i < 5; i++){
+    $(".output1").append(`<ul><ul><li>` +
+    listings[i].name + `</li><li>`+ listings[i].street + ` ` + listings[i].street2 + `</li>
+    <li>` + listings[i].city + `, ` + listings[i].state + ` ` + listings[i].zip + `</li>
+    <li>phone number: ` + listings[i].number + `</li>
+    <li>website: ` + listings[i].website + `</li>
+    <li>accepts new patients: ` + listings[i].patients + `</li></ul>`);
   }
 }
--- src/main.js
+++ src/main.js
@@ -4,12 +4,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
 import { Symptom } from './symptom.js';
 import { Listing } from './listing.js';
+import { getListings } from './listing.js';
 
 $(document).ready(function(){
   $('.searchForm').submit(function(event){
     event.preventDefault();
     let symptomInput = $('input#symptom').val();
-
 
     (async () => {
       let symptom = new Symptom();
@@ -17,171 +17,5 @@ $(document).ready(function(){
       symptom.getSymptom(symptomInput);
       getListings(response);
     })();
-
-    function getListings(response) {
-      let listings = [];
-      response.data.forEach(function(doctor){
-        listings.push(new Listing(
-        doctor.practices[0].name,
-        doctor.practices[0].visit_address.street,
-        doctor.practices[0].visit_address.street2,
-        doctor.practices[0].visit_address.city,
-        doctor.practices[0].visit_address.state,
-        doctor.practices[0].visit_address.zip,
-        doctor.practices[0].phones[0].number,
-        doctor.practices[0].website,
-        doctor.practices[0].accepts_new_patients,
-          )
-        );
-
-
-        });
-       for (let i = 0; i < 5; i++){
-         $(".output1").append(`<ul><ul><li>` +
-        listings[i].name + `</li><li>`+ listings[i].street + listings[i].street2 + `</li>
-        <li>` + listings[i].city + `, ` + listings[i].state + ` ` + listings[i].zip + `</li>
-        <li>phone number: ` + listings[i].number + `</li>
-        <li>website: ` + listings[i].website + `</li>
-        <li>accepts new patients: ` + listings[i].patients + `</li></ul>`);
-        }
-      }
-
-  // function outputListings(listings) {
-  //   for (let i = 0; i < 5; i++){
-  //     $(".output1").append(`<ul>The doctors who can save your life are:<li>` +
-  //       listings[i].name + `</li>
-  //       <li>`+ listings[i].street + listings[i].street2 + `</li>
-  //       <li>` + listings[i].city + `, ` + listings[i].state + ` ` + listings[i].zip + `</li>
-  //       <li>phone number: ` + listings[i].number + `</li>
-  //       <li>website: ` + listings[i].website + `</li>
-  //       <li>accepts new patients: ` + listings[i].patients + `</li></ul>`);
-  //   });
-  // }
-    //
-    // function outputListings(listings) {
-    //   for (let i = 0; i < 5; i++){
-    //     $(".output1").append(`<ul>The doctors who can save your life are:<li>` +
-    //       listings[i].name + `</li>
-    //     <li>`+ listings[i].street + listings[i].street2 + `</li>
-    //     <li>` + listings[i].city + `, ` + listings[i].state + ` ` + listings[i].zip + `</li>
-    //     <li>phone number: ` + listings[i].number + `</li>
-    //     <li>website: ` + listings[i].website + `</li>
-    //     <li>accepts new patients: ` + listings[i].patients + `</li></ul>`);
-    //   }
-    // }
-
-
-
   });
 });
-
-
-
-
-    //   let listing1 = new Listing(
-    //     `${response.data[0].practices[0].name}`,
-    //     `${response.data[0].practices[0].visit_address.street}`,
-    //     `, ${response.data[0].practices[0].visit_address.street2}`,
-    //     `${response.data[0].practices[0].visit_address.city}`,
-    //     `${response.data[0].practices[0].visit_address.state}`,
-    //     `${response.data[0].practices[0].visit_address.zip}`,
-    //     `${response.data[0].practices[0].phones[0].number}`,
-    //     `${response.data[0].practices[0].website}`,
-    //     `${response.data[0].practices[0].accepts_new_patients}`,
-    //   );
-    //
-    //
-    //   $(".output1").html(`<ul>The doctors who can save your life are:<ul><li>` +
-    //     listing1.name + `</li>
-    //   <li>`+ listing1.street + listing1.street2 + `</li>
-    //   <li>` + listing1.city + `, ` + listing1.state + ` ` + listing1.zip + `</li>
-    //   <li>phone number: ` + listing1.number + `</li>
-    //   <li>website: ` + listing1.website + `</li>
-    //   <li>accepts new patients: ` + listing1.patients + `</li>`);
-    //
-    //   let listing2 = new Listing(
-    //     `${response.data[1].practices[0].name}`,
-    //     `${response.data[1].practices[0].visit_address.street}`,
-    //     `, ${response.data[1].practices[0].visit_address.street2}`,
-    //     `${response.data[1].practices[0].visit_address.city}`,
-    //     `${response.data[1].practices[0].visit_address.state}`,
-    //     `${response.data[1].practices[0].visit_address.zip}`,
-    //     `${response.data[1].practices[0].phones[0].number}`,
-    //     `${response.data[1].practices[0].website}`,
-    //     `${response.data[1].practices[0].accepts_new_patients}`,
-    //   );
-    //
-    //
-    //   $(".output2").html(`<ul>The doctors who can save your life are:<ul><li>` +
-    //     listing2.name + `</li>
-    //   <li>`+ listing2.street + listing2.street2 + `</li>
-    //   <li>` + listing2.city + `, ` + listing2.state + ` ` + listing2.zip + `</li>
-    //   <li>phone number: ` + listing2.number + `</li>
-    //   <li>website: ` + listing2.website + `</li>
-    //   <li>accepts new patients: ` + listing2.patients + `</li>`);
-    //
-    //   let listing3 = new Listing(
-    //     `${response.data[2].practices[0].name}`,
-    //     `${response.data[2].practices[0].visit_address.street}`,
-    //     `, ${response.data[2].practices[0].visit_address.street2}`,
-    //     `${response.data[2].practices[0].visit_address.city}`,
-    //     `${response.data[2].practices[0].visit_address.state}`,
-    //     `${response.data[2].practices[0].visit_address.zip}`,
-    //     `${response.data[2].practices[0].phones[0].number}`,
-    //     `${response.data[2].practices[0].website}`,
-    //     `${response.data[2].practices[0].accepts_new_patients}`,
-    //   );
-    //
-    //
-    //   $(".output3").html(`<ul>The doctors who can save your life are:<ul><li>` +
-    //     listing3.name + `</li>
-    //   <li>`+ listing3.street + listing3.street2 + `</li>
-    //   <li>` + listing3.city + `, ` + listing3.state + ` ` + listing3.zip + `</li>
-    //   <li>phone number: ` + listing3.number + `</li>
-    //   <li>website: ` + listing3.website + `</li>
-    //   <li>accepts new patients: ` + listing3.patients + `</li>`);
-    //
-    //   let listing4 = new Listing(
-    //     `${response.data[3].practices[0].name}`,
-    //     `${response.data[3].practices[0].visit_address.street}`,
-    //     `, ${response.data[3].practices[0].visit_address.street2}`,
-    //     `${response.data[3].practices[0].visit_address.city}`,
-    //     `${response.data[3].practices[0].visit_address.state}`,
-    //     `${response.data[3].practices[0].visit_address.zip}`,
-    //     `${response.data[3].practices[0].phones[0].number}`,
-    //     `${response.data[3].practices[0].website}`,
-    //     `${response.data[3].practices[0].accepts_new_patients}`,
-    //   );
-    //
-    //
-    //   $(".output4").html(`<ul>The doctors who can save your life are:<ul><li>` +
-    //     listing4.name + `</li>
-    //   <li>`+ listing4.street + listing4.street2 + `</li>
-    //   <li>` + listing4.city + `, ` + listing4.state + ` ` + listing4.zip + `</li>
-    //   <li>phone number: ` + listing4.number + `</li>
-    //   <li>website: ` + listing4.website + `</li>
-    //   <li>accepts new patients: ` + listing4.patients + `</li>`);
-    //
-    //   let listing5 = new Listing(
-    //     `${response.data[4].practices[0].name}`,
-    //     `${response.data[4].practices[0].visit_address.street}`,
-    //     `, ${response.data[4].practices[0].visit_address.street2}`,
-    //     `${response.data[4].practices[0].visit_address.city}`,
-    //     `${response.data[4].practices[0].visit_address.state}`,
-    //     `${response.data[4].practices[0].visit_address.zip}`,
-    //     `${response.data[4].practices[0].phones[0].number}`,
-    //     `${response.data[4].practices[0].website}`,
-    //     `${response.data[4].practices[0].accepts_new_patients}`,
-    //   );
-    //
-    //
-    //   $(".output5").html(`<ul>The doctors who can save your life are:<ul><li>` +
-    //     listing5.name + `</li>
-    //   <li>`+ listing5.street + listing5.street2 + `</li>
-    //   <li>` + listing5.city + `, ` + listing5.state + ` ` + listing5.zip + `</li>
-    //   <li>phone number: ` + listing5.number + `</li>
-    //   <li>website: ` + listing5.website + `</li>
-    //   <li>accepts new patients: ` + listing5.patients + `</li>`);
-    // }
-  // });
-// });
